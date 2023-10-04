b34fd2a56554979fe0783249108ab8c271fcbc0a
Alisson Rodrigo <alissorodrigo098@gmail.com>
2023-10-03 22:45:09 -0300

    ok

--- exercicios_basicos/switch/ex1.c
+++ exercicios_basicos/switch/ex1.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
-int main () {
+int main() {
     
+    int num[5];
+    int i = 0;
 
+    while (i < 5) { // Correção: Use parênteses em torno de i < 5
+        printf("Digite um numero: ");
+        scanf("%d", &num[i]);
+        i++;
+    }
 
+    // Resto do seu código aqui...
 
+    return 0;
 }
