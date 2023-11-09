54277efc78566856eeb42412c7766ad12f8b6b35
Alisson Rodrigo <alissorodrigo098@gmail.com>
2023-11-08 22:05:48 -0300

    as

--- questoes/llist_atvd_TAF/quest3/main.c
+++ questoes/llist_atvd_TAF/quest3/main.c
@@ -1,48 +1,63 @@
-#include "livros.c"
+#include "livros.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
-    int QtdLivros;
-    char titulo[50];
-    char palavra_chave[50];
-    char autor[50];
+    int QtdLivros, opcao;
+    Livros *livros = NULL;
 
-    printf("Quantos livros deseja inserir? ");
+    printf("Quantos livros deseja cadastrar? ");
     scanf("%d", &QtdLivros);
+    livros = atribuir(QtdLivros);
 
-    Livros *livros = atribuir(QtdLivros);
+    do {
+        printf("\nMenu de Acessos:\n");
+        printf("1. Imprimir informações dos livros\n");
+        printf("2. Adicionar palavras-chave a um livro\n");
+        printf("3. Buscar livros por palavra-chave\n");
+        printf("4. Imprimir livros de um autor\n");
+        printf("5. Remover palavra-chave de um livro\n");
+        printf("6. Sair\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
 
-    if (livros == NULL) {
-        printf("Erro na alocação de memória\n");
-        return 1;
-    }
+        switch (opcao) {
+            case 1:
+                imprimir(livros, QtdLivros);
+                break;
+            case 2:
+                char titulo[50];
+                printf("Digite o título do livro: ");
+                scanf("%s", titulo);
+                adicionar_palavra_chave(livros, titulo, QtdLivros);
+                break;
+            case 3:
+                char palavra_chave[100];
+                printf("Digite a palavra-chave que deseja buscar: ");
+                scanf("%s", palavra_chave);
+                buscar_palavras_chave(livros, palavra_chave, QtdLivros);
+                break;
+            case 4:
+                char autor[50];
+                printf("Digite o nome do autor: ");
+                scanf("%s", autor);
+                imprimir_livros_autor(livros, autor, QtdLivros);
+                break;
+            case 5:
+                char palavra_chave_remover[100];
+                printf("Digite a palavra-chave que deseja remover: ");
+                scanf("%s", palavra_chave_remover);
+                remover_palavra_chave(livros, palavra_chave_remover, QtdLivros);
+                break;
+            case 6:
+                break;
+            default:
+                printf("Opção inválida. Tente novamente.\n");
+        }
+    } while (opcao != 6);
 
-    imprimir(livros, QtdLivros);
-
-    //printf("\nDigite o nome do livro que deseja adicionar palavra-chave: ");
-    //scanf("%s", titulo);
-
-    //adicionar_palavra_chave(livros, titulo, QtdLivros);
-
-    //imprimir(livros, QtdLivros);
-
-    //printf("Digite a palavra chave que deseja buscar: ");
-    //scanf ("%s", palavra_chave);
-    
-    //buscar_palavras_chave(livros, palavra_chave, QtdLivros);
-
-    //printf ("Digite o nome do autor: ");
-    //scanf ("%s", autor);
-
-    //imprimir_livros_autor(livros, autor, QtdLivros);
-
-    printf ("Digite uma palavra chave");
-    scanf ("%s", palavra_chave);
-
-    remover_palavra_chave(livros, palavra_chave, QtdLivros);
-
-    imprimir (livros, QtdLivros);
+    liberar(livros);
 
     return 0;
 }
