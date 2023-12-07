0d5111a7a102bf0d2df82707b48de26153978f5d
Alisson Rodrigo <alissorodrigo098@gmail.com>
2023-12-07 10:56:33 -0300

    ok

---
+++ questoes/lista_circular/book.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "book.h"
+
+
+struct book {
+    char autor[50];
+    char titulo[50];
+    int ano;
+    int id;
+    Book *next;
+}
+
+Book *zerar_lista() {
+    return NULL;
+}
+
+Book *inserir_ordenado_circular (Book *l, char *titulo, char *autor, int ano, int id) {
+
+}
+
---
+++ questoes/lista_circular/book.h
@@ -0,0 +1,4 @@
+typedef struct book Book;
+
+Book *zerar_lista();
+Book *inserir_ordenado_circular (Book *l, char *titulo, char *autor, int ano, int id);
