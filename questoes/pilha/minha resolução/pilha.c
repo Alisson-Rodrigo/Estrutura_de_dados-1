909e146e4259ab48e0b431e2a13f9935175dbed6
Alisson Rodrigo <alissorodrigo098@gmail.com>
2024-01-25 07:52:13 -0300

    as

--- questoes/pilha/minha resolução/pilha.c
+++ questoes/pilha/minha resolução/pilha.c
@@ -14,43 +14,45 @@ struct pilha {
 };
 
 Pilha* pilha_cria (void) {
-    Pilha *topo = (Pilha*) malloc (sizeof(Pilha));
-    topo->prim = NULL;
-    return topo;
+	Pilha* p = (Pilha*) malloc(sizeof(Pilha));
+	p->prim = NULL;
+	return p;
 }
 
 void pilha_push (Pilha* p, float v) {
-	No *novo = (No*) malloc (sizeof(novo));
+	No* novo = (No*) malloc (sizeof(No));
 	novo->info = v;
 	novo->prox = p->prim;
 	p->prim = novo;
+
 }
 
 void pilha_pop (Pilha* p){
 	No* t;
-	float v;
-	if (pilha_vazia(p)) { 
-		printf("Pilha vazia.\n");
+	if (p->prim == NULL) {
+		printf ("A pilha está vazia");
 		exit(1);
 	}
+
 	t = p->prim;
 	p->prim = t->prox;
-	free(t);
+	free (t);
+
 }
 
 int pilha_vazia (Pilha* p){
-	if (p->prim == NULL)
-		return 1;
-	return 0;
+	if (p->prim == NULL) {
+		return 0;
+	}
+	return 1;
+
 }
 
 void mostrar_pilha(Pilha* p) {
 	No *t;
 	t = p->prim;
 	while (t != NULL) {
-		printf ("%.2f\n", t->info);
+		printf ("Valores: %.2f", t->info);
 		t = t->prox;
 	}
-	
-	
 }
