16ffaac5ff0cc179c22df33240e795dea28a4ff5
Alisson Rodrigo <alissorodrigo098@gmail.com>
2024-02-15 09:26:28 -0300

    s

--- testes_ordencao/main.c
+++ testes_ordencao/main.c
@@ -13,10 +13,45 @@ void insert(int vetor[],int tam){
         }
         vetor[j+1] = aux;
     }
+}
 
-    for (i=0;i<tam;i++) {
-        printf ("%d ", vetor[i]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void insert(int vetor[],int tam){
+    int i, j, aux;
+    for (i=1;i<tam;i++) {
+    aux = vetor[i];
+    j = i - 1;
+    while (j >= 0 && aux < vetor[j])
+    {
+        vetor[j+!] = vetor[j];
+        j--; 
     }
+    vetor[j+1] = aux;
+    }
+
 }
 
 int main () {
