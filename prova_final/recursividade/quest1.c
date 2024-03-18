0a936a03b4cc136c328b0646c3a60add180a3cfc
Alisson Rodrigo <alissorodrigo098@gmail.com>
2024-03-18 20:09:08 -0300

    as

--- prova_final/recursividade/quest1.c
+++ prova_final/recursividade/quest1.c
@@ -4,13 +4,14 @@
 void contar_n(int n) {
     if (n == 0) {
         printf ("%d", n);
+        return; // Adicionando a condição de parada
     }
     contar_n(n-1);
     printf ("%d", n);
-
 
 } 
 
 int main() {
     contar_n(10);
+    return 0; // Adicionando uma declaração de retorno para main
 }
